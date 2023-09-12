8517bb45dc225a0b1cf30b7288b0dfcdf301b5fd
borja-sc <borja.sc95@gmail.com>
2023-09-12 05:02:39 +0200

    Modify basic_cleaning to filter invalid coordinates

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -29,6 +29,11 @@ def go(args):
     logger.info("Changing lats_review type to date")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    logger.info("Filtering valid longitude and latitude values")
+    idx = df['longitude'].between(-74.25, -
+                                  73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Saving clean data")
     df.to_csv("clean_sample.csv", index=False)
 
