9fa5edfb4f01823752dc6a27f0f3fb3727ff6d4c
borja-sc <borja.sc95@gmail.com>
2023-09-12 05:07:25 +0200

    Formatting changes

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -30,8 +30,7 @@ def go(args):
     df['last_review'] = pd.to_datetime(df['last_review'])
 
     logger.info("Filtering valid longitude and latitude values")
-    idx = df['longitude'].between(-74.25, -
-                                  73.50) & df['latitude'].between(40.5, 41.2)
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
 
     logger.info("Saving clean data")
